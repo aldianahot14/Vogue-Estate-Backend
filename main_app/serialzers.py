c81168e7be22f7c9562b7e3fb726c32a57074244
cd <lesleyrotonto@gmail.com>
2024-02-10 20:14:39 -0500

    add views and add a serializer that is missing for single listing view

--- main_app/serialzers.py
+++ main_app/serialzers.py
@@ -1,23 +1,29 @@
 from rest_fromework import serializers
 from .models import Client, Agent, Listing
 
-class ListingSerializer(serializers.ModelSerializer):
+class AgentListingsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
 
-class AgentSerializer(serializers.ModelSerializer):
+class AgentSerializer(serializers.HyperlinkedModelSerializer):
     # this needs to be defined in the Agent model
     has_listing = serializers.SerializerMethodField()
-    listings = ListingSerializer(many=True, read_only=True)
+    listings = AgentListingsSerializer(many=True, read_only=True)
     class Meta:
         model = Agent
         fields = '__all__'
     
     def get_has_listing(self, obj):
         return obj.has_listing()
-    
-class ClientSerializer(serializers.ModelSerializer):
+
+# added this so you can see all listings 
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+class ClientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
