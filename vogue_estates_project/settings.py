a382c80ca5bec194f07c76cc5ab1208184b5a97d
cd <lesleyrotonto@gmail.com>
2024-02-15 09:43:38 -0500

    moved cors up in settings

--- vogue_estates_project/settings.py
+++ vogue_estates_project/settings.py
@@ -91,13 +91,14 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 # CORS_ORIGIN_WHITELIST=[ "http://localhost:5173",]
